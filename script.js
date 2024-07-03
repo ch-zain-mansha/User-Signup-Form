eaa59be3a32bcc8cbfcbca9f15ffd6d20eba6da2
ch-zain-mansha <ch.zainulabidin.mansha@gmail.com>
2024-07-03 18:09:28 +0500

    user signup form

--- script.js
+++ script.js
@@ -1,169 +1,183 @@
 //input
-let input1 = document.getElementById('i1');
-let input2 = document.getElementById('i2');
-let input3 = document.getElementById('i3');
-let input4 = document.getElementById('i4');
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+let input3 = document.getElementById('input3');
+let input4 = document.getElementById('input4');
 
 //para
-let paraForUserName = document.getElementById('p1');
-let paraForUserName2 = document.getElementById('p11');
-let paraForEmail = document.getElementById('p2');
-let paraForEmail2 = document.getElementById('p21');
-let paraForPassword = document.getElementById('p3');
-let paraForPassword2 = document.getElementById('p31');
-let paraForPassword3 = document.getElementById('p32');
-let paraForPassword4 = document.getElementById('p33');
-let paraForPassword5 = document.getElementById('p34');
-let paraForPassword6 = document.getElementById('p35');
-let paraForPassword7 = document.getElementById('p36');
-let paraForConPassword = document.getElementById('p4');
-let paraForConPassword2 = document.getElementById('p41');
+let paraForUserName = document.getElementById('paragraphForUsername');
+let paraForUserName2 = document.getElementById('paragraphForUsername2');
+let paraForUserName3 = document.getElementById('paragraphForUsername3');
+let paraForEmail = document.getElementById('paragraphForEmail');
+let paraForEmail2 = document.getElementById('paragraphForEmail2');
+let paraForPassword = document.getElementById('paragraphForPassword');
+let paraForPassword2 = document.getElementById('paragraphForPassword2');
+let paraForPassword3 = document.getElementById('paragraphForPassword3');
+let paraForPassword4 = document.getElementById('paragraphForPassword4');
+let paraForPassword5 = document.getElementById('paragraphForPassword5');
+let paraForPassword6 = document.getElementById('paragraphForPassword6');
+let paraForPassword7 = document.getElementById('paragraphForPassword7');
+let paraForConPassword = document.getElementById('paragraphForConfirmPassword');
 
 //functions
 let verifyUserName = () => {
-    let username = input1.value.trim();
-
+    let username = input1.value;
     if (username === "") {
-        paraForUserName.innerText = "Write your name";
-        paraForUserName.style.color = 'red';
-        paraForUserName.innerText = 'Username should not contain uppercase letters';
-        paraForUserName.style.color = 'red';
-        paraForUserName2.innerText = 'Spaces are not allowed';
-        paraForUserName2.style.color = 'red';
+        paraForUserName.classList.add('dotRed');
+        paraForUserName.classList.remove('dotGreen');
+        paraForUserName2.classList.add('dotRed');
+        paraForUserName2.classList.remove('dotGreen');
+        paraForUserName3.classList.add('dotRed');
+        paraForUserName3.classList.remove('dotGreen');
     }
     else if (username.match(/[A-Z]/) && username.includes(" ")) {
-        paraForUserName.innerText = 'Username should not contain uppercase letters';
-        paraForUserName.style.color = 'red';
-        paraForUserName2.innerText = 'Spaces are not allowed';
-        paraForUserName2.style.color = 'red';
+        paraForUserName.classList.add('dotRed');
+        paraForUserName.classList.remove('dotGreen');
+        paraForUserName2.classList.add('dotRed');
+        paraForUserName2.classList.remove('dotGreen');
+        paraForUserName.innerText = 'Username is not correct';
+        paraForUserName.classList.add('dotRed');
+        paraForUserName.classList.remove('dotGreen');
     }
     else if (username.match(/[A-Z]/)) {
-        paraForUserName.innerText = 'Username should not contain uppercase letters';
-        paraForUserName.style.color = 'red';
-        paraForUserName2.innerText = 'Spaces are not allowed';
-        paraForUserName2.style.color = 'green';
+        paraForUserName.innerText = 'Username is not correct';
+        paraForUserName.classList.add('dotRed');
+        paraForUserName.classList.remove('dotGreen');
+        paraForUserName2.classList.add('dotRed');
+        paraForUserName3.classList.add('dotGreen');
+        paraForUserName3.classList.remove('dotRed');
+    }
+    else if (username.match(/[-]/)) {
+        paraForUserName.style.display='none';
+        paraForUserName2.innerHTML='Minus is not allowed';
+        paraForUserName2.classList.add('dotRed');
+        paraForUserName3.style.display='none';
     }
     else if (username.includes(" ")) {
-        paraForUserName.innerText = 'Username should not contain uppercase letters';
-        paraForUserName.style.color = 'green';
-        paraForUserName2.innerText = 'Spaces are not allowed';
-        paraForUserName2.style.color = 'red';
+        paraForUserName.innerText = 'Username is not correct';
+        paraForUserName.classList.add('dotRed');
+        paraForUserName.classList.remove('dotGreen');
+        paraForUserName2.classList.add('dotGreen');
+        paraForUserName2.classList.remove('dotRed');
+        paraForUserName3.classList.add('dotRed');
+        paraForUserName3.classList.remove('dotGreen');
     }
     else {
-        paraForUserName.innerText = 'Username should not contain uppercase letters';
-        paraForUserName.style.color = 'green';
-        paraForUserName2.innerText = 'Spaces are not allowed';
-        paraForUserName2.style.color = 'green';
+        paraForUserName3.classList.add('dotGreen');
+        paraForUserName3.classList.remove('dotRed');
+        paraForUserName2.classList.add('dotGreen');
+        paraForUserName2.classList.remove('dotRed');
+        paraForUserName.innerText = 'Username is correct';
+        paraForUserName.classList.add('dotGreen');
+        paraForUserName.classList.remove('dotRed');
 }
 }
 
 
 let verifyEmail = () => {
-    paraForEmail.innerText = "";
-    paraForEmail.style.color = 'black';
 
-    const email = input2.value.trim();
+    const email = input2.value;
     const emailCheck = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]/; 
     
     if (email === "") {
-        paraForEmail.innerText += "Write your email\n";
-        paraForEmail.style.color = 'red';
+        paraForEmail.classList.add('dotRed');
+        paraForEmail.classList.remove('dotGreen');
     } else {
-        paraForEmail.innerText += "Write your email\n";
-        paraForEmail.style.color = 'green';
+        paraForEmail.style.display = 'none';
     }
 
     if (!emailCheck.test(email)) {
-        paraForEmail.innerText += 'Invalid Email Format\n';
-        paraForEmail.style.color = 'red';
+        paraForEmail2.classList.add('dotRed');
+        paraForEmail2.classList.remove('dotGreen');
     } else {
-        paraForEmail.innerText += 'Invalid Email Format\n';
-        paraForEmail.style.color = 'green';
+        paraForEmail2.innerText = 'Email is valid'
+        paraForEmail2.classList.add('dotGreen');
+        paraForEmail2.classList.remove('dotRed');
     }
 }
 
 
 let verifyPassword = () => {
-    paraForPassword.innerText = "";
-    paraForPassword2.innerText = "";
-    paraForPassword3.innerText = "";
-    paraForPassword4.innerText = "";
-    paraForPassword5.innerText = "";
-    paraForPassword6.innerText = "";
-
     
-    if (input3.value.trim() === "") {
-        paraForPassword.innerText += "Write your password\n";
-        paraForPassword.style.color = 'red';
+    if (input3.value === "") {
+        paraForPassword.classList.add('dotRed');
+        paraForPassword.classList.remove('dotGreen');
     } else {
-        paraForPassword.innerText += "Write your password\n";
-        paraForPassword.style.color = 'green';
+        paraForPassword.style.display = 'none';
     }
 
     if (input3.value.includes(" ")) {
-        paraForPassword.innerText += 'Spaces are not allowed\n';
-        paraForPassword2.style.color = 'red';
-    } else {
-        paraForPassword2.innerText += 'Spaces are not allowed\n';
-        paraForPassword2.style.color = 'green';
+        paraForPassword2.classList.remove('dotGreen');
+        paraForPassword2.classList.add('dotRed');
+    } 
+    else if(input3.value === ""){
+        paraForPassword2.classList.remove('dotGreen');
+        paraForPassword2.classList.add('dotRed');
+    }
+    else {
+        paraForPassword2.classList.remove('dotRed');
+        paraForPassword2.classList.add('dotGreen');
     }
 
     if (input3.value.length < 8 || input3.value.length > 16) {
-        paraForPassword3.innerText += 'Password must be between 8 and 16 characters\n';
-        paraForPassword3.style.color = 'red';
+        paraForPassword3.classList.remove('dotGreen');
+        paraForPassword3.classList.add('dotRed');
     } else {
-        paraForPassword3.innerText += 'Password must be between 8 and 16 characters\n';
-        paraForPassword3.style.color = 'green';
+        paraForPassword3.classList.remove('dotRed');
+        paraForPassword3.classList.add('dotGreen');
     }
 
     if (!/[A-Z]/.test(input3.value)) {
-        paraForPassword4.innerText += 'Password must contain at least one uppercase letter\n';
-        paraForPassword4.style.color = 'red';
+        paraForPassword4.classList.remove('dotGreen');
+        paraForPassword4.classList.add('dotRed');
     } else {
-        paraForPassword4.innerText += 'Password must contain at least one uppercase letter\n';
-        paraForPassword4.style.color = 'green';
+        paraForPassword4.classList.remove('dotRed');
+        paraForPassword4.classList.add('dotGreen');
     }
 
     if (!/[a-z]/.test(input3.value)) {
-        paraForPassword5.innerText += 'Password must contain at least one lowercase letter\n';
-        paraForPassword5.style.color = 'red';
+        paraForPassword5.classList.remove('dotGreen');
+        paraForPassword5.classList.add('dotRed');
     } else {
-        paraForPassword5.innerText += 'Password must contain at least one lowercase letter\n';
-        paraForPassword5.style.color = 'green';
+        paraForPassword5.classList.remove('dotRed');
+        paraForPassword5.classList.add('dotGreen');
     }
 
     if (!/\d/.test(input3.value)) {
-        paraForPassword6.innerText += 'Password must contain at least one digit\n';
-        paraForPassword6.style.color = 'red';
+        paraForPassword6.classList.remove('dotGreen');
+        paraForPassword6.classList.add('dotRed');
     } else {
-        paraForPassword6.innerText += 'Password must contain at least one digit\n';
-        paraForPassword6.style.color = 'green';
+        paraForPassword6.classList.remove('dotRed');
+        paraForPassword6.classList.add('dotGreen');
     }
 
     if (!/[*&^%$#@!]/.test(input3.value)) {
-        paraForPassword6.innerText += 'Password must contain at least one symbol\n';
-        paraForPassword6.style.color = 'red';
+        paraForPassword7.classList.remove('dotGreen');
+        paraForPassword7.classList.add('dotRed');
     } else {
-        paraForPassword6.innerText += 'Password must contain at least one symbol\n';
-        paraForPassword6.style.color = 'green';
+        paraForPassword7.classList.remove('dotRed');
+        paraForPassword7.classList.add('dotGreen');
     }
 }
 
 let confirmPassword = () => {
-    if (input4.value.trim() === "") {
+    if (input4.value === "") {
         paraForConPassword.innerText = "Confirm your password";
-        paraForConPassword.style.color = 'red'
+        paraForConPassword.classList.add('dotRed');
+        paraForConPassword.classList.remove('dotGreen');
     } else if (input4.value !== input3.value) {
         paraForConPassword.innerText = 'Passwords do not match';
-        paraForConPassword.style.color = 'red'
+        paraForConPassword.classList.add('dotRed');
+        paraForConPassword.classList.remove('dotGreen');
     } else {
         paraForConPassword.innerText = 'Password match';
-        paraForConPassword.style.color = 'green'
+        paraForConPassword.classList.add('dotGreen');
+        paraForConPassword.classList.remove('dotRed');
     }
 }
 
 // Form submitting
-let submit = document.getElementById("sub");
+let submit = document.getElementById("submit");
 submit.addEventListener('click', (e) => {
     e.preventDefault();
     verifyUserName();
@@ -171,3 +185,18 @@ submit.addEventListener('click', (e) => {
     verifyPassword();
     confirmPassword();
 });
+
+
+// Keypress for inputs
+let inputForUsername = document.getElementById('input1').addEventListener('input', () => {
+        verifyUserName();
+    });
+    let inputForEmail = document.getElementById('input2').addEventListener('input', () => {
+        verifyEmail();
+    });
+    let inputForPassword = document.getElementById('input3').addEventListener('input', () => {
+        verifyPassword();
+    });
+    let inputForConfirmPassword = document.getElementById('input4').addEventListener('input', () => {
+        confirmPassword();
+    });
